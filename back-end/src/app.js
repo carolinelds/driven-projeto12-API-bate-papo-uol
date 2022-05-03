9e2320e45909821a1fefd3e8fcfe85c88c196125
Caroline LS <carolinelds@gmail.com>
2022-05-03 07:18:54 -0300

    Configura endpoint para enviar mensagens com POST

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -13,17 +13,17 @@ dotenv.config();
 
 let database = null;
 dotenv.config();
-const mongoClient = new MongoClient(process.env.MONGO_URI); 
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
-const promise = mongoClient.connect(); 
+const promise = mongoClient.connect();
 
 promise.then(response => {
-	database = mongoClient.db("uol"); 
-	console.log(chalk.blue.bold("Banco de dados conectado com sucesso"));
+    database = mongoClient.db("uol");
+    console.log(chalk.blue.bold("Banco de dados conectado com sucesso"));
 });
 
 promise.catch(err => {
-	console.log(chalk.red.bold("Falha na conexão com o banco de dados"), err)
+    console.log(chalk.red.bold("Falha na conexão com o banco de dados"), err)
 });
 
 /*
@@ -43,12 +43,12 @@ const mensagem = {
 }
 */
 
-app.post("/participants", async (req,res) => {    
+app.post("/participants", async (req, res) => {
     const nameSchema = joi.object({
         name: joi.string().required()
     });
-    const validation  = nameSchema.validate(req.body, { abortEarly: true });
-    if (validation.error){
+    const validation = nameSchema.validate(req.body, { abortEarly: true });
+    if (validation.error) {
         console.log(validation.error.details);
         res.sendStatus(422);
         return;
@@ -58,7 +58,7 @@ app.post("/participants", async (req,res) => {
         name: req.body.name,
         lastStatus: Date.now()
     };
-    
+
     let now = dayjs();
     now = now.format('HH:mm:ss');
 
@@ -69,7 +69,7 @@ app.post("/participants", async (req,res) => {
         type: 'status',
         time: now
     };
-    
+
     try {
         const nomeExiste = await database.collection("participantes").find({ name: novoParticipante.name }).toArray();
         if (nomeExiste.length !== 0) {
@@ -77,28 +77,70 @@ app.post("/participants", async (req,res) => {
             res.sendStatus(409);
             return;
         }
-        
+
         await database.collection("participantes").insertOne(novoParticipante);
-        await database.collection("mensagens").insertOne(novaMensagem);        
+        await database.collection("mensagens").insertOne(novaMensagem);
 
         console.log(chalk.green.bold("Login feito com sucesso"));
         res.sendStatus(201);
-    
-    } catch(err) {
+
+    } catch (err) {
         console.log(chalk.red.bold("Erro inesperado no servidor"));
         res.sendStatus(500);
     }
 })
 
-app.get("/participants", async (req,res) => {
+app.get("/participants", async (req, res) => {
     try {
         const participantes = await database.collection("participantes").find({}).toArray();
         res.send(participantes);
-    } catch(err) {
+    } catch (err) {
         res.sendStatus(500);
     }
 });
 
+app.post("/messages", async (req, res) => {
+    const mensagemSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.any().valid('message', 'private_message')
+    });
 
+    const validation = mensagemSchema.validate(req.body, { abortEarly: true });
+    if (validation.error) {
+        console.log(validation.error.details);
+        res.sendStatus(422);
+        return;
+    }
+
+    let now = dayjs();
+    now = now.format('HH:mm:ss');
+
+    const novaMensagem = {
+        to: req.body.to,
+        text: req.body.text,
+        type: req.body.type,
+        from: req.headers.user,
+        time: now
+    };
+
+    try {
+        const participanteExiste = await database.collection("participantes").find({ name: novaMensagem.from }).toArray();
+        if (participanteExiste.length === 0) {
+            console.log(chalk.red.bold("Esse usuário não existe"));
+            res.sendStatus(422);
+            return;
+        }
+
+        await database.collection("mensagens").insertOne(novaMensagem);
+
+        console.log(chalk.green.bold("Mensagem enviada com sucesso"));
+        res.sendStatus(201);
+
+    } catch (err) {
+        console.log(chalk.red.bold("Falha no envio da mensagem"));
+        res.sendStatus(422);
+    }
+})
 
 app.listen(5000, () => console.log("Server is running."));
